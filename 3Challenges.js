8769008ed304925c3f4d48a945ca265d0abfb4be
Chris Laflamme <Chris Laflamme>
2017-01-15 18:40:46 -0500

    Adding 3 challenges file

---
+++ 3Challenges.js
@@ -0,0 +1,5 @@
+/*
+1. Player loses ENTIRE score when he rools two 6s in a row.  Then switch player.
+2. Add an input field to the HTML where players can set the winning score.
+3. Add another dice to the game, so that there are two dice. The player loses his current score when one of this is a 1.
+*/
